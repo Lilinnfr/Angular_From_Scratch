310d07a846e97d37a4aaf6e664ef057fc6323edd
Lydie CHAUMET <lydie.chaumet@ouidou.fr>
2023-11-16 17:16:49 +0100

    Conditions par attribut

--- src/directives/phone-number.directive.ts
+++ src/directives/phone-number.directive.ts
@@ -1,6 +1,8 @@
 export class PhoneNumberDirective {
   static selector = "[phone-number]";
 
+  willHaveSpaces = true;
+
   constructor(public element: HTMLElement) {}
 
   formatPhoneNumber(element: HTMLInputElement) {
@@ -12,11 +14,15 @@ export class PhoneNumberDirective {
       groups.push(value.substring(i, i + 2));
     }
 
-    element.value = groups.join(" ");
+    element.value = groups.join(this.willHaveSpaces ? " " : "");
   }
 
   init() {
     this.element.style.borderColor = "red";
+
+    if (this.element.hasAttribute("with-spaces")) {
+      this.willHaveSpaces = this.element.getAttribute("with-spaces") === "true";
+    }
 
     this.element.addEventListener("input", (event) => {
       this.formatPhoneNumber(event.target as HTMLInputElement);
